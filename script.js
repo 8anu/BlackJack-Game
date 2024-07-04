543594e1fd3ec3a311599d06d2fd5976040a30db
Hemant Vyas <110166331+8anu@users.noreply.github.com>
2024-07-04 22:58:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let firstCard;
+let secondCard;
+let sum;
+let msg = "";
+
+function startGame() {
+  firstCard = parseInt(prompt("Enter card value: "), 10);
+  secondCard = parseInt(prompt("Enter card value: "), 10);
+  sum = firstCard + secondCard;
+
+  document.getElementById("cards").textContent = firstCard + " " + secondCard;
+  document.getElementById("sum").textContent = sum;
+
+  if (sum < 21) {
+    msg = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    msg = "Wohoo! You've got Blackjack!";
+  } else {
+    msg = "You're out of the game!";
+  }
+
+  console.log(msg);
+
+  document.getElementById("result").textContent = msg;
+}
