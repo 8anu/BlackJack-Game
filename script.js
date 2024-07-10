7df5e03414a8aa81c280c9119cc76f687679d077
Hemant Vyas <110166331+8anu@users.noreply.github.com>
2024-07-10 23:14:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let firstCard;
 let secondCard;
+let newCard;
 let sum;
 let msg = "";
 
@@ -23,3 +24,29 @@ function startGame() {
 
   document.getElementById("result").textContent = msg;
 }
+
+
+function nCard(){
+  newCard = parseInt(prompt("Enter new card value: "), 10);
+
+  sum = sum + newCard;
+
+  document.getElementById("cards").textContent = firstCard + " " + secondCard + " " + newCard;
+
+  document.getElementById("sum").textContent = sum;
+
+  if (sum < 21) {
+    msg = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    msg = "Wohoo! You've got Blackjack!";
+  } else {
+    msg = "You're out of the game!";
+  }
+
+  console.log(msg);
+
+  document.getElementById("result").textContent = msg;
+  
+}
+
+
